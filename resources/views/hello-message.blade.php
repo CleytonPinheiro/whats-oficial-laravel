ffc0f04b60a22da05d6c1d7dd7741966b4dddb1e
Cleyton <42612444+CleytonPinheiro@users.noreply.github.com>
2022-09-16 21:12:30 -0300

    foto channel

--- resources/views/hello-message.blade.php
+++ resources/views/hello-message.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Controle de Mensagens do Whatsapp</title>
+    <title>Gerenciamento CRMVIA whatsapp</title>
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-    <h1> AGUARDE. Em breve a Channel chega para lhe ajudar.
-        GAEL
-    </h1>
+    <h1> AGUARDE. Em breve a Channel chega para lhe ajudar. GAEL </h1>
+
+    <img src="/assets/images/channel.jpeg" alt="">
     
 </body>
 </html>
